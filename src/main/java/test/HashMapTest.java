d97a709f7710e7ef16136be57f1255108e6564c8
HANXU2018 <1076998404@qq.com>
2021-04-02 22:29:23 +0800

    HashMapDemo

---
+++ src/main/java/test/HashMapTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+
+        hashMap.put("1","2");
+        hashMap.put("3","4");
+        System.out.println(hashMap.put("5","6"));
+        System.out.println(hashMap.put("1","3"));
+
+        System.out.println(hashMap);
+    }
+}
